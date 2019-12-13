c64dc19b4f5f4aa8c5792974036818cbc0c4fd74
Wazlyy <58254367+Wazlyy@users.noreply.github.com>
2019-12-13 14:35:42 +0100

    Add files via upload

---
+++ Les-Envies-de-Mu/bouclesoreilles.php
@@ -0,0 +1,287 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Les Envies De Mû - Boucles d'oreilles</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+
+  <?php include('header.php'); ?>
+
+    <div class="blocg">
+    <div class="titreg"><h2>boucles d'oreilles</h2></div><br>
+
+    <div class="flex">
+
+    <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="80%"></h5>
+        <p class="card-text">TITRE PRODUIT</p>
+<button type="button" class="btn commbutt" data-toggle="modal" data-target="#exampleModalScrollable">
+  + INFO
+</button>
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">TITRE DU PRODUIT</h5>
+        </button>
+      </div>
+      <div class="modal-body">
+       PRIX : ...<br>
+       MATERIAUX : ...<br>
+       DISPONIBILITÉ : ...<br>
+       CODE PRODUIT : ...<br>
+       POID: ...<br>
+       COULEUR: ...<br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="80%"></h5>
+        <p class="card-text">TITRE PRODUIT</p>
+<button type="button" class="btn commbutt" data-toggle="modal" data-target="#exampleModalScrollable">
+  + INFO
+</button>
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">TITRE DU PRODUIT</h5>
+        </button>
+      </div>
+      <div class="modal-body">
+       PRIX : ...<br>
+       MATERIAUX : ...<br>
+       DISPONIBILITÉ : ...<br>
+       CODE PRODUIT : ...<br>
+       POID: ...<br>
+       COULEUR: ...<br>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="80%"></h5>
+        <p class="card-text">TITRE PRODUIT</p>
+<button type="button" class="btn commbutt" data-toggle="modal" data-target="#exampleModalScrollable">
+  + INFO
+</button>
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">TITRE DU PRODUIT</h5>
+        </button>
+      </div>
+      <div class="modal-body">
+       PRIX : ...<br>
+       MATERIAUX : ...<br>
+       DISPONIBILITÉ : ...<br>
+       CODE PRODUIT : ...<br>
+       POID: ...<br>
+       COULEUR: ...<br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="80%"></h5>
+        <p class="card-text">TITRE PRODUIT</p>
+<button type="button" class="btn commbutt" data-toggle="modal" data-target="#exampleModalScrollable">
+  + INFO
+</button>
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">TITRE DU PRODUIT</h5>
+        </button>
+      </div>
+      <div class="modal-body">
+       PRIX : ...<br>
+       MATERIAUX : ...<br>
+       DISPONIBILITÉ : ...<br>
+       CODE PRODUIT : ...<br>
+       POID: ...<br>
+       COULEUR: ...<br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="80%"></h5>
+        <p class="card-text">TITRE PRODUIT</p>
+<button type="button" class="btn commbutt" data-toggle="modal" data-target="#exampleModalScrollable">
+  + INFO
+</button>
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">TITRE DU PRODUIT</h5>
+        </button>
+      </div>
+      <div class="modal-body">
+       PRIX : ...<br>
+       MATERIAUX : ...<br>
+       DISPONIBILITÉ : ...<br>
+       CODE PRODUIT : ...<br>
+       POID: ...<br>
+       COULEUR: ...<br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="80%"></h5>
+        <p class="card-text">TITRE PRODUIT</p>
+<button type="button" class="btn commbutt" data-toggle="modal" data-target="#exampleModalScrollable">
+  + INFO
+</button>
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">TITRE DU PRODUIT</h5>
+        </button>
+      </div>
+      <div class="modal-body">
+       PRIX : ...<br>
+       MATERIAUX : ...<br>
+       DISPONIBILITÉ : ...<br>
+       CODE PRODUIT : ...<br>
+       POID: ...<br>
+       COULEUR: ...<br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="80%"></h5>
+        <p class="card-text">TITRE PRODUIT</p>
+<button type="button" class="btn commbutt" data-toggle="modal" data-target="#exampleModalScrollable">
+  + INFO
+</button>
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">TITRE DU PRODUIT</h5>
+        </button>
+      </div>
+      <div class="modal-body">
+       PRIX : ...<br>
+       MATERIAUX : ...<br>
+       DISPONIBILITÉ : ...<br>
+       CODE PRODUIT : ...<br>
+       POID: ...<br>
+       COULEUR: ...<br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-3">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="80%"></h5>
+        <p class="card-text">TITRE PRODUIT</p>
+<button type="button" class="btn commbutt" data-toggle="modal" data-target="#exampleModalScrollable">
+  + INFO
+</button>
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-scrollable" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalScrollableTitle">TITRE DU PRODUIT</h5>
+        </button>
+      </div>
+      <div class="modal-body">
+       PRIX : ...<br>
+       MATERIAUX : ...<br>
+       DISPONIBILITÉ : ...<br>
+       CODE PRODUIT : ...<br>
+       POID: ...<br>
+       COULEUR: ...<br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">FERMER</button>
+      </div>
+    </div>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+
+        </div>
+
+    </div>
+
+
+  <?php include('footer.php'); ?>
+
+</body>
---
+++ Les-Envies-de-Mu/header.php
@@ -0,0 +1,62 @@
+                <div class="container-fluid">
+                <div class="row">
+                  <div class="col-0 col-sm-1 col-md-0 col-lg-1 col-xl-4 hg">
+                  </div>
+                  <div class="col-5 col-sm-4 col-md-6 col-lg-7 col-xl-4 logo">
+                    <a href="index.php"><img id="logo1" src="./img/logo.jpg" alt="" width="430px" height="230px"></a>
+                    <a href="index.php"><img id="logo2" src="./img/logo.jpg" alt="" width="430px" height="230px"></a>
+                    <a href="index.php"><img id="logo3" src="./img/logo.jpg" alt="" width="230px" height="130px"></a>
+                  </div>
+                  <div class="col-7 col-sm-9 col-md-5 col-lg-3 col-xl-4 hd">
+        <nav class="navbar navbar-light navbar-1 white">
+                <a class="navbar-brand" href="#"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent15"
+                  aria-controls="navbarSupportedContent15" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent15">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Produits</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Évènements</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Comités d'Entreprises</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ventes à domicile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Ou nous trouver</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contactez-moi</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+                      <div class="nbp">
+                          <input class="form-control mr-sm-2" type="search" placeholder="RECHERCHE" aria-label="Search">
+                          <div id="facebook"><a href="#"><img src="../img/facebook.png" alt="facebook" width="35px" height="35px" class="norm"></a>
+                          </div>
+                          <div id="instagram"><a href="#"><img src="../img/instagram.png" alt="facebook" width="35px" height="35px" class="norm"></a>
+                          </div>
+                      </div>
+                    </div>
+                </div>
+                        <div class="header">
+                            <div class="dbut"><a href="index.php">Accueil</a></div>
+                            <div class="dbut"><a href="produits.php">Produits</a></div>
+                            <div class="dbut"><a href="evenements.php">Évènements</a></div>
+                            <div class="dbut"><a href="comitesentreprises.php">Comités d'Entreprises</a></div>
+                            <div class="dbut"><a href="venteadomicile.php">Ventes à domicile</a></div>
+                            <div class="dbut"><a href="ounoustrouver.php">Où nous trouver</a></div>
+                            <div class="dbut"><a href="faq.php">FAQ</a></div>
+                            <div class="dbut"><a href="contact.php">Contactez-moi</a></div>
+                        </div>
