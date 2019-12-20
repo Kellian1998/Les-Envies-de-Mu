9f202212b6c86d23d3ea9aac264179f0e9f844e1
Wazlyy <58254367+Wazlyy@users.noreply.github.com>
2019-12-20 15:54:11 +0100

    Add files via upload

---
+++ DOSSIER FINAL/description.php
@@ -0,0 +1,24 @@
+<div class="blocg">
+               
+  <div id="pdppresent"></div>
+
+    <div class="disblockd">
+
+      <div class="titre">les envies de mû</div>
+
+      <div class="textpresent">
+        
+      Coucou, je suis Muriel, créatrice située à Montastruc-La-Conseillère.<br> Après avoir testé le pastel et la peinture à relief sur tableau, j’ai découvert la résine sur toile, et ça a été le coup de cœur. J’adore cette matière qui permet autant de champs des possibles!
+      Étant une fan inconditionnelle de bijoux, j’ai tout naturellement appliqué la technique de la résine dans la création de bijoux.<br>
+      La résine est associée au verre, à des métaux précieux, des pierres fines... qui permet des inclusions de toutes sortes: feuilles d’or ou d’argent, peinture à effet, papier de soie, tissus, pigments colorés, petits objets, etc.
+      Résistante, lumineuse, flamboyante, aux couleurs riches et profondes, cette matière permet de réaliser de grosses pièces tout en gardant une légèreté.
+      <br>Les bijoux des Envies de Mû sont des modèles uniques en acier inoxydable, en laiton sans nickel, sans plomb et sans cadium. Certains modèles sont en plaqué or ou plaqué argent. 
+      <br>Il est possible de personnaliser votre bijou aussi bien dans le choix d’inclusion dans la résine (avec une de vos photos, une couleur, tissus, objets...),  que dans le montage (choix de la matière du support, pampilles, connecteurs...).
+      Les Envies de Mû, c’est vous faire plaisir tout en réalisant vos envies et mes envies...
+      Les Envies de Mû, le bijou qui vous révèle! C’est la petite touche sur une tenue... mais qui change tout!</p>
+                  
+      </div>
+
+  </div>
+
+</div>
---
+++ DOSSIER FINAL/une.php
@@ -0,0 +1,58 @@
+<div class="blocg" id="respsc">
+
+<div class="disblock" id="respnone">
+
+<div class="titre" id="respnone">les produits à la une</div>
+
+<div class="textpresent" id="respnone">
+  
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            
+</div>
+
+</div>
+
+                
+    <div class="ealu">
+
+    <a href="#"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="100%"></div></a>
+
+    <div class="aludesc">
+    <div class="aludescl"><img src="./img/ic1.png" width="50%" height="70%" class="norm"></div>Catégorie
+    </div>
+
+    <div class="aludescd">
+    <div class="aludescl"><img src="./img/ic2.png" width="50%" height="70%" class="norm"></div>Description
+    </div>
+
+    </div>
+
+    <div class="ealu">
+
+    <a href="#"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="100%"></div></a>
+
+    <div class="aludesc">
+    <div class="aludescl"><img src="./img/ic1.png" width="50%" height="70%" class="norm"></div>Catégorie
+    </div>
+
+    <div class="aludescd">
+    <div class="aludescl"><img src="./img/ic2.png" width="50%" height="70%" class="norm"></div>Description
+    </div>
+
+    </div>
+    
+    <div class="ealu">
+
+    <a href="#"><div class="ealui"><img src="./img/bg.jpg" width="100%" height="100%"></div></a>
+
+    <div class="aludesc">
+    <div class="aludescl"><img src="./img/ic1.png" width="50%" height="70%" class="norm"></div>Catégorie
+    </div>
+
+    <div class="aludescd">
+    <div class="aludescl"><img src="./img/ic2.png" width="50%" height="70%" class="norm"></div>Description
+    </div>
+
+    </div>
+
+  </div>
